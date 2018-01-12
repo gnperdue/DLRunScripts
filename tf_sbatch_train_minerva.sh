8f2578be2a0ebbff0789a198e7dfb796bca9677c
Gabriel Perdue <perdue@fnal.gov>
2018-01-12 10:19:54 -0600

    start to include scripts for wilson

---
+++ tf_sbatch_train_minerva.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCRIPTKEY=`date +%s`
+CONFIGFILE=Configs/tf_mnv_st_epsilon_wilson_cluster_training.cfg
+python mnv_tf_script_gen.py $CONFIGFILE $SCRIPTKEY
+
+NGPU=1
+NODES=gpu2
+NODES=gpu4
+
+# show what we will do...
+cat << EOF
+sbatch --gres=gpu:${NGPU} --nodelist=${NODES} job${SCRIPTKEY}.sh
+EOF
+
+# do the thing, etc.
+pushd job${SCRIPTKEY}
+cat job${SCRIPTKEY}.sh >& job_log.txt
+# sbatch --gres=gpu:${NGPU} --nodelist=${NODES} job${SCRIPTKEY}.sh
+popd
+
