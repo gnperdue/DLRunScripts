006d0b8da11e11de59c132c8bea3eeb787d1c7a6
Gabriel Perdue <perdue@fnal.gov>
2018-01-26 17:45:44 -0600

    proc NX me1Amc

--- tf_slurm_process_hdf5_to_tfrecord.sh
+++ tf_slurm_process_hdf5_to_tfrecord.sh
@@ -19,8 +19,8 @@ OUTDIR="/data/perdue/minerva/tensorflow/data/${PROCESSING}/${SAMPLE}"
 LOGFILE="log_hdf5_to_tfrec_minerva_xtxutuvtv${SCRIPTKEY}.txt"
 HDF5TYPE="vtxfndingimgs"
 HDF5TYPE="hadmultkineimgs"
-FILEPAT="${HDF5TYPE}_127x94_${SAMPLE}"
 FILEPAT="${HDF5TYPE}_127x94_${SAMPLE}_tiny"
+FILEPAT="${HDF5TYPE}_127x94_${SAMPLE}"
 
 # file creation parameters
 NEVTS=20000
@@ -31,8 +31,8 @@ TRAINFRAC=0.0
 VALIDFRAC=0.0
 TRAINFRAC=0.88
 VALIDFRAC=0.06
-TESTREAD=""
 TESTREAD="--test_read"
+TESTREAD=""
 
 # which singularity image
 SNGLRTY="/data/perdue/singularity/tf_1_4.simg"
