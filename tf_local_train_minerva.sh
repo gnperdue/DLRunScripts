77f2d518876d46989c964d21a7eeb5062a3b723d
Gabriel Perdue <gnperdue@me.com>
2018-02-01 15:52:36 -0600

    run hadmult

--- tf_local_train_minerva.sh
+++ tf_local_train_minerva.sh
@@ -3,6 +3,7 @@
 SCRIPTKEY=`date +%s`
 CONFIGFILE=Configs/tf_mnv_st_epsilon_local_iMac2017_training.cfg
 CONFIGFILE=Configs/tf_mnv_st_epsilon_local_iMac2017_prediction.cfg
+CONFIGFILE=Configs/tf_mnv_hadmult_local_iMac2017_training.cfg
 
 python mnv_tf_script_gen.py $CONFIGFILE $SCRIPTKEY
 
