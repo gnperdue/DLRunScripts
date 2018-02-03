55f74466d6aff87357a2dbae093f2aed9d598c4b
Gabriel Perdue <perdue@fnal.gov>
2018-02-03 15:21:06 -0600

    take out path hack

--- tf_slurm_process_hdf5_to_tfrecord.sh
+++ tf_slurm_process_hdf5_to_tfrecord.sh
@@ -29,7 +29,7 @@ VALIDFRAC=0.06
 FILEPAT="${HDF5TYPE}_127x94_${SAMPLE}"
 FILEPAT="hadmultkineimgs_127x94_${SAMPLE}"
 HDF5DIR="/data/perdue/minerva/hdf5/${PROCESSING}"
-OUTDIR="/data/perdue/minerva/tensorflow/data/${PROCESSING}/${SAMPLE}_vtx"
+OUTDIR="/data/perdue/minerva/tensorflow/data/${PROCESSING}/${SAMPLE}"
 LOGFILE="log_hdf5_to_tfrec_minerva_xtxutuvtv${SCRIPTKEY}.txt"
 
 # file creation parameters
