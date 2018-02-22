afd584b3659ad86b0e6bf0395bde882d42050263
Gabriel Perdue <gnperdue@me.com>
2018-02-22 14:27:00 -0600

    rename some config files for better clarity

--- tf_local_train_minerva.sh
+++ tf_local_train_minerva.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 SCRIPTKEY=`date +%s`
-CONFIGFILE=Configs/tf_mnv_st_epsilon_local_iMac2017_prediction.cfg
-CONFIGFILE=Configs/tf_mnv_hadmult_local_iMac2017_prediction.cfg
-CONFIGFILE=Configs/tf_mnv_hadmult_local_iMac2017_training.cfg
-CONFIGFILE=Configs/tf_mnv_st_epsilon_local_iMac2017_training.cfg
-CONFIGFILE=Configs/tf_mnv_st_epsilon_local_iMac2017_training_menndl_633167.cfg
+CONFIGFILE=Configs/tf_mnv_vtxfindr_local_iMac2017_prediction.cfg
+CONFIGFILE=Configs/tf_mnv_hadmultp_local_iMac2017_prediction.cfg
+CONFIGFILE=Configs/tf_mnv_hadmultp_local_iMac2017_training.cfg
+CONFIGFILE=Configs/tf_mnv_vtxfindr_local_iMac2017_training.cfg
+CONFIGFILE=Configs/tf_mnv_vtxfindr_local_iMac2017_training_menndl_633167.cfg
 
 python mnv_tf_script_gen.py $CONFIGFILE $SCRIPTKEY
 
