d9fc65cdd585a7c04e48206fa279a38401ee4f82
Gabriel Perdue <perdue@fnal.gov>
2018-02-22 14:24:10 -0600

    rename some configs for clarity, ease of reading during an `ls`

--- tf_sbatch_train_minerva.sh
+++ tf_sbatch_train_minerva.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 SCRIPTKEY=`date +%s`
-CONFIGFILE=Configs/tf_mnv_st_epsilon_wilson_cluster_testing.cfg
-CONFIGFILE=Configs/tf_mnv_st_epsilon_wilson_cluster_prediction.cfg
-CONFIGFILE=Configs/tf_mnv_hadmult_wilson_cluster_training_AB.cfg
-CONFIGFILE=Configs/tf_mnv_st_epsilon_wilson_cluster_training_AB.cfg
-CONFIGFILE=Configs/tf_mnv_st_epsilon_wilson_cluster_training_E_menndl_633167.cfg
-CONFIGFILE=Configs/tf_mnv_st_epsilon_wilson_cluster_training_Atargbal.cfg
+CONFIGFILE=Configs/tf_mnv_vtxfindr_wilson_cluster_testing.cfg
+CONFIGFILE=Configs/tf_mnv_vtxfindr_wilson_cluster_prediction.cfg
+CONFIGFILE=Configs/tf_mnv_hadmultp_wilson_cluster_training_AB.cfg
+CONFIGFILE=Configs/tf_mnv_vtxfindr_wilson_cluster_training_AB.cfg
+CONFIGFILE=Configs/tf_mnv_vtxfindr_wilson_cluster_training_E_menndl_633167.cfg
+CONFIGFILE=Configs/tf_mnv_vtxfindr_wilson_cluster_training_Atargbal.cfg
 python mnv_tf_script_gen.py $CONFIGFILE $SCRIPTKEY
 
 NGPU=1
