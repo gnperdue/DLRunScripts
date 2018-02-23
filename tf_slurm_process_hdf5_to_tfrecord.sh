940dd626497bef0314b8489585fb2e9f91e39cac
Gabriel Perdue <perdue@fnal.gov>
2018-02-23 16:38:51 -0600

    me1Emc starting at 79

--- tf_slurm_process_hdf5_to_tfrecord.sh
+++ tf_slurm_process_hdf5_to_tfrecord.sh
@@ -11,7 +11,7 @@ JOBDIR=`pwd`
 
 # file logistics
 PROCESSING="201801"
-STARTIDX=0
+STARTIDX=79
 TESTREAD="--test_read"
 TESTREAD=""
 
@@ -21,8 +21,8 @@ TFRECSTRUCTURE="mnvimgs"
 TRAINFRAC=0.0
 VALIDFRAC=0.0
 
-PLAYLIST="me1Emc_targets_bal"
 PLAYLIST="me1Lmc"
+PLAYLIST="me1Emc_targets_bal"
 HDF5TYPE="hadmultkineimgs"
 TFRECSTRUCTURE="hadmultkineimgs"
 TRAINFRAC=0.88
