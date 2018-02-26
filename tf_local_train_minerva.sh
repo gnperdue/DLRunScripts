f513b7a44532975ca73b02bb201994090f9e9ac7
Gabriel Perdue <gnperdue@me.com>
2018-02-26 13:43:21 -0600

    batch norm and reularizer strategies are in network def dict now

--- tf_local_train_minerva.sh
+++ tf_local_train_minerva.sh
@@ -3,9 +3,9 @@
 SCRIPTKEY=`date +%s`
 CONFIGFILE=Configs/tf_mnv_vtxfindr_local_iMac2017_prediction.cfg
 CONFIGFILE=Configs/tf_mnv_hadmultp_local_iMac2017_training.cfg
-CONFIGFILE=Configs/tf_mnv_vtxfindr_local_iMac2017_training.cfg
 CONFIGFILE=Configs/tf_mnv_vtxfindr_local_iMac2017_prediction.cfg
 CONFIGFILE=Configs/tf_mnv_vtxfindr_local_iMac2017_training_menndl_633167.cfg
+CONFIGFILE=Configs/tf_mnv_vtxfindr_local_iMac2017_training.cfg
 
 python mnv_tf_script_gen.py $CONFIGFILE $SCRIPTKEY
 
