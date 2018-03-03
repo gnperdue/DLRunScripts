2b1cce0437289f3511cd90c61286ca2f7cece1e9
Gabriel Perdue <perdue@fnal.gov>
2018-03-03 10:24:55 -0600

    train an AB Eroica+ model with same conv. padding

--- tf_sbatch_train_minerva.sh
+++ tf_sbatch_train_minerva.sh
@@ -2,7 +2,6 @@
 
 SCRIPTKEY=`date +%s`
 CONFIGFILE=Configs/tf_mnv_hadmultp_wilson_cluster_training_AB.cfg
-CONFIGFILE=Configs/tf_mnv_vtxfindr_wilson_cluster_training_AB.cfg
 CONFIGFILE=Configs/tf_mnv_vtxfindr_wilson_cluster_training_Atargbal.cfg
 CONFIGFILE=Configs/tf_mnv_vtxfindr_wilson_cluster_training_E.cfg
 CONFIGFILE=Configs/tf_mnv_vtxfindr_wilson_cluster_training_E_menndl_633167.cfg
@@ -10,6 +9,7 @@ CONFIGFILE=Configs/tf_mnv_vtxfindr_wilson_cluster_training_Etargbal.cfg
 CONFIGFILE=Configs/tf_mnv_vtxfindr_wilson_cluster_predict_Bmc_using_E.cfg
 CONFIGFILE=Configs/tf_mnv_vtxfindr_wilson_cluster_predict_Bdata_using_E.cfg
 CONFIGFILE=Configs/tf_mnv_vtxfindr_wilson_cluster_prediction_using_ABGEroicaPlus.cfg
+CONFIGFILE=Configs/tf_mnv_vtxfindr_wilson_cluster_training_AB.cfg
 python mnv_tf_script_gen.py $CONFIGFILE $SCRIPTKEY
 
 NGPU=1
