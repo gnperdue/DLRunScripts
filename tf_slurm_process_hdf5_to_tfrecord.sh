772832ac5791e9ebad8231b7f30b869a43170464
Gabriel Perdue <perdue@fnal.gov>
2018-03-07 09:19:22 -0600

    1D data convert, 86+

--- tf_slurm_process_hdf5_to_tfrecord.sh
+++ tf_slurm_process_hdf5_to_tfrecord.sh
@@ -11,15 +11,9 @@ JOBDIR=`pwd`
 
 # file logistics
 PROCESSING="201801"
-STARTIDX=0
+STARTIDX=86
 TESTREAD="--test_read"
 TESTREAD=""
-
-PLAYLIST="me1Mdata"
-HDF5TYPE="mnvimgs"
-TFRECSTRUCTURE="mnvimgs"
-TRAINFRAC=0.0
-VALIDFRAC=0.0
 
 PLAYLIST="me1Emc_targets_bal"
 PLAYLIST="me1Pmc"
@@ -27,6 +21,12 @@ HDF5TYPE="hadmultkineimgs"
 TFRECSTRUCTURE="hadmultkineimgs"
 TRAINFRAC=0.88
 VALIDFRAC=0.06
+
+PLAYLIST="me1Ddata"
+HDF5TYPE="mnvimgs"
+TFRECSTRUCTURE="mnvimgs"
+TRAINFRAC=0.0
+VALIDFRAC=0.0
 
 INPUTFILEPAT="${HDF5TYPE}_127x94_${PLAYLIST}"
 HDF5DIR="/data/perdue/minerva/hdf5/${PROCESSING}"
