3f9396cb245bf6c5f4f4df36a437befe259eea5d
Gabriel Perdue <gnperdue@me.com>
2018-03-16 11:02:59 -0500

    1B MC missing files

--- tf_local_process_hdf5_to_tfrecord.sh
+++ tf_local_process_hdf5_to_tfrecord.sh
@@ -15,7 +15,7 @@ TFRECSTRUCTURE="mnvimgs"
 TRAINFRAC=0.0
 VALIDFRAC=0.0
 
-PLAYLIST="me1Amc_missingfiles"
+PLAYLIST="me1Bmc_missingfiles"
 HDF5TYPE="hadmultkineimgs"
 TFRECSTRUCTURE="hadmultkineimgs"
 TRAINFRAC=0.88
