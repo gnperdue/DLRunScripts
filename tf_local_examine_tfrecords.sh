33d855228835789eb1acf8e205826bc8f08cf3a9
Gabriel Perdue <gnperdue@me.com>
2018-03-19 14:30:55 -0500

    add check field arg

--- tf_local_examine_tfrecords.sh
+++ tf_local_examine_tfrecords.sh
@@ -3,9 +3,15 @@
 SCRIPTKEY=`date +%s`
 mkdir -p job${SCRIPTKEY}
 
+PLAYLIST="me1Adata"
+TFRECTYPE="mnvimgs"
+
+PLAYLIST="me1Amc_missingfiles"
+TFRECTYPE="hadmultkineimgs"
+
 PROCESSING="201801"
 BASEP="${HOME}/Documents/MINERvA/AI/minerva_tf"
-DATADIR="${BASEP}/tfrec2/${PROCESSING}"
+DATADIR="${BASEP}/tfrec/${PROCESSING}/${PLAYLIST}"
 LOGFILE="log_examine_tfrec${SCRIPTKEY}.txt"
 OUTPAT="result_examine_tfrec${SCRIPTKEY}"
 
@@ -15,16 +21,13 @@ NPLANECODES=173
 PLANECODES="--n_planecodes $NPLANECODES"
 IMGPAR="--imgw_x $IMGWX --imgw_uv $IMGWUV"
 
-PLAYLIST="me1Adata"
-TFRECTYPE="mnvimgs"
-
-PLAYLIST="me1Amc"
-TFRECTYPE="vtxfndingimgs"
-TFRECTYPE="hadmultkineimgs"
-
 FILEPAT="${TFRECTYPE}_127x${IMGWX}_${PLAYLIST}"
 
-ARGS="--data_dir $DATADIR --file_root $FILEPAT --compression gz --log_name $LOGFILE --out_pattern $OUTPAT $PLANECODES $IMGPAR --tfrec_type $TFRECTYPE"
+# default is eventids
+CHECKFIELD="--field n_hadmultmeas"
+CHECKFIELD=""
+
+ARGS="--data_dir $DATADIR --file_root $FILEPAT --compression gz --log_name $LOGFILE --out_pattern $OUTPAT $PLANECODES $IMGPAR --tfrec_type $TFRECTYPE $CHECKFIELD"
 
 cat << EOF
 python tfrec_examiner.py $ARGS
