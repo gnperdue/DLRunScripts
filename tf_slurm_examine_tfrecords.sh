6ef155d55438d61cc6bd780ac2a2a12d0716f3de
Gabriel Perdue <perdue@fnal.gov>
2018-03-20 12:38:34 -0500

    1F MC, 156+

--- tf_slurm_examine_tfrecords.sh
+++ tf_slurm_examine_tfrecords.sh
@@ -13,7 +13,7 @@ JOBDIR=`pwd`
 PLAYLIST="me1Amc"
 TFRECTYPE="hadmultkineimgs"
 
-PLAYLIST="me1Adata_missingfiles"
+PLAYLIST="me1Bdata"
 TFRECTYPE="mnvimgs"
 
 FILEPAT="${TFRECTYPE}_127x94_${PLAYLIST}"
