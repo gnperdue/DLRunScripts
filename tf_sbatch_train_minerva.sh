7cbdf55290dfb32ba610f1ecb62b8b5752047373
Gabriel Perdue <perdue@fnal.gov>
2018-03-26 09:19:59 -0500

    run the EF model training

--- tf_sbatch_train_minerva.sh
+++ tf_sbatch_train_minerva.sh
@@ -4,8 +4,8 @@ SCRIPTKEY=`date +%s`
 CONFIGFILE=Configs/tf_mnv_hadmultp_wilson_cluster_training_AB.cfg
 CONFIGFILE=Configs/tf_mnv_vtxfindr_wilson_cluster_training_Etargbal.cfg
 CONFIGFILE=Configs/tf_mnv_vtxfindr_wilson_cluster_prediction_using_ABGEroicaPlus.cfg
-CONFIGFILE=Configs/tf_mnv_vtxfindr_wilson_cluster_training_EF.cfg
 CONFIGFILE=Configs/tf_mnv_vtxfindr_wilson_cluster_training_ABCDG.cfg
+CONFIGFILE=Configs/tf_mnv_vtxfindr_wilson_cluster_training_EF.cfg
 python mnv_tf_script_gen.py $CONFIGFILE $SCRIPTKEY
 
 NGPU=1
