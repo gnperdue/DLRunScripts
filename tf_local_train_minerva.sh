40126cc73eb75a42b553989effa8535352041b31
Gabriel Perdue <gnperdue@me.com>
2018-08-28 15:19:28 -0500

    test hdf5 input for inference

--- tf_local_train_minerva.sh
+++ tf_local_train_minerva.sh
@@ -6,6 +6,7 @@ CONFIGFILE=Configs/tf_mnv_vtxfindr_local_iMac2017_prediction_using_E.cfg
 CONFIGFILE=Configs/tf_mnv_vtxfindr_local_iMac2017_training.cfg
 CONFIGFILE=Configs/tf_mnv_vtxfindr_local_iMac2017_prediction_check_using_LOP.cfg
 CONFIGFILE=Configs/tf_mnv_vtxfindr_local_iMac2017_mc_prediction_using_LOP.cfg
+CONFIGFILE=Configs/tf_mnv_vtxfindr_local_iMac2017_mc_prediction_using_LOP_hdf5.cfg
 
 python mnv_tf_script_gen.py $CONFIGFILE $SCRIPTKEY
 
